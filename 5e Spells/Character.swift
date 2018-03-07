28cf61737fd786c7ed61f8e1291beeaf6b6a11ce
Mikael Olezeski <mikaelolezeski@Mikaels-MacBook-Pro.local>
2018-03-06 22:23:07 -0500

    Added delete

---
+++ 5e Spells/Character.swift
@@ -0,0 +1,9 @@
+//
+//  Character.swift
+//  5e Spells
+//
+//  Created by Mikael Olezeski on 3/3/18.
+//  Copyright © 2018 Mikael Olezeski. All rights reserved.
+//
+
+import Foundation
---
+++ 5e Spells/SpellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SpellTableViewCell.swift
+//  5e Spells
+//
+//  Created by Mikael Olezeski on 2/28/18.
+//  Copyright © 2018 Mikael Olezeski. All rights reserved.
+//
+
+import UIKit
+
+class SpellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
